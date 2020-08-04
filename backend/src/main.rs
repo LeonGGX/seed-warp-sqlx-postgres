8d512aee86a373b40f9ddfcee1aa43aba8233098
LeonGGX <37880486+LeonGGX@users.noreply.github.com>
2020-08-04 10:36:56 +0000

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -44,7 +44,7 @@ async fn modify_person() {
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
 
-    let db_url = "postgres://postgres:1922leon@localhost/persons";
+    let db_url = "postgres://postgres:password@localhost/persons";
     let pool = db::create_pg_pool(db_url).await.unwrap();
     let api = filters::person_filters(pool);
 
