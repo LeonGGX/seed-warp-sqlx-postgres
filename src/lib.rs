683ae9eea6c1570b775f3145ff922fc6cf36db0a
LeonGGX <37880486+LeonGGX@users.noreply.github.com>
2020-08-04 10:34:14 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 // src/lib.rs
 
 use serde::{Deserialize, Serialize};
-//use sqlx::{FromRow, PgPool, Row};
 use std::str::FromStr;
 
-//use sqlx::postgres::PgRow;
-//use warp::reply::Response;
 use std::fmt::Formatter;
 use std::fmt;
 
@@ -50,13 +47,6 @@ impl FromStr for InsertablePerson {
     }
 }
 
-/*
-impl warp::reply::Reply for InsertablePerson {
-    fn into_response(self) -> Response {
-        Response::new(format!("nom: {}\n prenom: {}", self.first_name, self.last_name).into())
-    }
-}
-*/
 // this struct will be used to represent database record
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Person {
@@ -65,21 +55,6 @@ pub struct Person {
     pub last_name: String,
 }
 
-/*
-// si on veut une sortie String et non Json ...
-// donc pas très utile.
-impl warp::reply::Reply for Person {
-    fn into_response(self) -> Response {
-        Response::new(
-            format!(
-                "id: {}\n nom: {}\n prenom: {}",
-                self.id, self.first_name, self.last_name
-            )
-            .into(),
-        )
-    }
-}
-*/
 impl Default for Person {
     fn default() -> Self {
         Self {
@@ -89,46 +64,3 @@ impl Default for Person {
         }
     }
 }
-
-
-/*
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-pub struct ListPersons {
-    pub list_persons: Vec<Person>,
-}
-
-impl ListPersons {
-    pub fn new(vec_pers: Vec<Person>) -> Self {
-        Self {
-            list_persons: vec_pers,
-        }
-    }
-
-    pub fn to_vec_string(&self) -> Vec<String> {
-        let list = self.clone();
-        let mut vec_str: Vec<String> = Vec::new();
-        for pers in list.list_persons {
-            vec_str.push(InsertablePerson::from_person(pers).to_string());
-        }
-        vec_str
-    }
-
-    pub fn vec_to_string(&self) -> String {
-        let mut str = String::new();
-        let str_vec = self.to_vec_string();
-        for pers in str_vec {
-            str.push_str(pers.as_ref());
-            str.push("\n".parse().unwrap());
-        }
-        str
-    }
-}
-
-impl Default for ListPersons {
-    fn default() -> Self {
-        Self {
-            list_persons: vec![],
-        }
-    }
-}
-*/
